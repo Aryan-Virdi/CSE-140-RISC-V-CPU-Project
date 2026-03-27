cd855bd0186dd8b372ae3c4ca8ce946a8ee14fc6
jalam01 <162780469+jalam01@users.noreply.github.com>
2026-03-26 21:17:28 -0700

    Update fetch.cpp

--- fetch.cpp
+++ fetch.cpp
@@ -1,14 +1,1 @@
-#include <cstdint>
 
-uint32_t fetch(uint32_t &pc, uint8_t memory[]) {
-    uint32_t instruction = 0;
-
-    instruction |= (uint32_t)memory[pc];
-    instruction |= (uint32_t)memory[pc + 1] << 8;
-    instruction |= (uint32_t)memory[pc + 2] << 16;
-    instruction |= (uint32_t)memory[pc + 3] << 24;
-
-    pc += 4;  // Move to next instruction
-
-    return instruction;
-}
