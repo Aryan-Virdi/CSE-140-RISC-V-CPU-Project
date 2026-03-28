0096417cd98f06a5e511edfad4381e12083388e9
Aryan Virdi <avirdi2@ucmerced.edu>
2026-03-28 14:47:30 -0700

    Added data memory logic.

---
+++ main/data_memory.hpp
@@ -0,0 +1,14 @@
+#ifndef DATA_MEMORY_HPP
+#define DATA_MEMORY_HPP 
+
+#include <cstdint>
+
+int retrieveMemory(int address, const int dataMemory[32]){
+    return dataMemory[address];
+}
+
+void storeMemory(int address, int dataMemory[32], int value){
+    dataMemory[address] = value;
+}
+
+#endif
--- main/main.cpp
+++ main/main.cpp
@@ -20,6 +20,9 @@ int PC = 0;
 // CPU's register file. 32 registers
 int rf[32];
 
+// Data Memory
+int d_mem[32];
+
 // Computer's instruction memory
 vector<uint32_t> instructionMemory;
 
@@ -27,7 +30,7 @@ vector<uint32_t> instructionMemory;
 int main(int argc, char* argv[]) {
 
     // All 32 registers initialized to zero.
-    for (int i = 0; i < 32; i++){ rf[i] = 0; }  // Global arrays should be initialized to zero automatically, but here it is done manually just in case.
+    for (int i = 0; i < 32; i++){ rf[i] = 0; d_mem[i] = 0; }  // Global arrays should be initialized to zero automatically, but here it is done manually just in case.
 
     string programFileName = argv[1];
     populateInstructionMemory(programFileName, instructionMemory);  // Populate instruction memory with program instructions.
