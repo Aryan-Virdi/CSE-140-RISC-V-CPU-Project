fec9fbcf05bd28f0cf70b7995426417a5d2dd1c2
Aryan Virdi <avirdi2@ucmerced.edu>
2026-03-29 18:21:56 -0700

    Added unified mem() as prescribed in the instructions.

--- main/data_memory.hpp
+++ main/data_memory.hpp
@@ -2,6 +2,7 @@
 #define DATA_MEMORY_HPP 
 
 #include <cstdint>
+#include "exit_codes.h"
 
 /*
     Parameter dataMemory[32]:   Read-only reference to the data memory array.
@@ -24,6 +25,28 @@ int retrieveMemory(const int dataMemory[32], int address){
 */
 void storeMemory(int dataMemory[32], int address, int value){
     dataMemory[(address / 4)] = value;
+}
+
+/*
+    Parameter dataMemory[32]:   A reference to the data memory array.
+    Parameter address:          An integer representing a memory address in the form of (4*i)
+    Parameter value:            An optional integer value to be written into memory. If unnecessary,
+                                a dummy value should be provided.
+    Parameter memWrite:         A true or false signal of whether the instruction is writing to memory
+                                or not.
+
+    Returns:                    Value held in memory at "address" if memWrite is false,
+                                A generic success code if memWrite is true.
+
+    Note:                       Assumes that address is only ever an integer multiple of 4.
+*/
+int mem(int dataMemory[32], int address, int value, bool memWrite){
+    if (memWrite == true){
+        storeMemory(dataMemory, address, value);
+        return SUCCESS;
+    } else {
+        return retrieveMemory(dataMemory, address);
+    }
 }
 
 #endif
