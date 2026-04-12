af7117e2e5b9466a36d1de1b0157b82d203edf74
Aryan Virdi <avirdi2@ucmerced.edu>
2026-04-12 13:56:17 -0700

    Added PC update events to fetching logic.

--- main/fetch.hpp
+++ main/fetch.hpp
@@ -7,19 +7,21 @@
 #include <stdexcept>
 
 #include "exit_codes.h"
+#include "printing.hpp"
 
 /*
     Parameter PC:                   Takes a reference to the global program counter. This will modify PC.
     Parameter instructionMemory:    Read-only reference to a vector, which should hold the instructions of the program.
     Parameter branchTarget:         A precomputed target address for an instruction in I-Memory.
     Parameter branchTaken:          Boolean flag to determine whether to increment PC or switch to target address.
+    Parameter printQueue:           A reference to the global queue for printing changes to PC and memory.
 
     Returns:                        Instruction in instructionMemory pointed to by PC.
 
-    Note:                           Assumes that (PC / 4) is only ever an integer. I.e., PC is either 0 or some 
-                                    multiple of 4
+    Note:                           Assumes that PC is only ever a non-negative integer multiple of 4. 
+                                    I.e., PC is either 0 or some multiple of 4.
 */
-uint32_t fetch(int& PC, const std::vector<uint32_t>& instructionMemory, uint32_t branchTarget, bool branchTaken){
+uint32_t fetch(int& PC, const std::vector<uint32_t>& instructionMemory, uint32_t branchTarget, bool branchTaken, PrintEvent& printQueue){
     int memoryIndex = PC / 4;
     if (!(memoryIndex < instructionMemory.size())){;
         std::cerr << "Exit Code: " << ERROR << std::endl;
@@ -30,6 +32,8 @@ uint32_t fetch(int& PC, const std::vector<uint32_t>& instructionMemory, uint32_t
     uint32_t nextPC = PC + 4;
 
     PC = (branchTaken ? branchTarget : nextPC);
+
+    printQueue.addPrintEvent(locationType::programCounter, EMPTY_IDX, PC);
 
     return instruction;
 }
