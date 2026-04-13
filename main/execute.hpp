2bd32fd497ffb78fdb1521ea260dc58b8bcec981
Aryan Virdi <avirdi2@ucmerced.edu>
2026-04-13 14:16:35 -0700

    Formatting.

--- main/execute.hpp
+++ main/execute.hpp
@@ -5,20 +5,27 @@ void execute(int operand1, int operand2, int alu_ctrl, int sign_extension_offset
     int result = 0;
 
     switch (alu_ctrl) {
-        case 0b0000: result = operand1 & operand2; 
-        break;
-        case 0b0001: result = operand1 | operand2; 
-        break;
-        case 0b0010: result = operand1 + operand2; 
-        break;
-        case 0b0110: result = operand1 - operand2; 
-        break;
-        case 0b0111: result = (operand1 < operand2) ? 1 : 0; 
-        break;
-        case 0b1100: result = ~(operand1 | operand2); 
-        break;
-        default: result = 0; 
-        break;
+        case 0b0000: 
+            result = operand1 & operand2; 
+            break;
+        case 0b0001: 
+            result = operand1 | operand2; 
+            break;
+        case 0b0010: 
+            result = operand1 + operand2; 
+            break;
+        case 0b0110: 
+            result = operand1 - operand2; 
+            break;
+        case 0b0111: 
+            result = (operand1 < operand2) ? 1 : 0; 
+            break;
+        case 0b1100: 
+            result = ~(operand1 | operand2); 
+            break;
+        default: 
+            result = 0; 
+            break;
     }
 
     alu_zero = (result == 0);
