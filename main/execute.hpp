e0d4c31771a7e1d6b41a08c00a11b4f77cac6304
Aryan Virdi <avirdi2@ucmerced.edu>
2026-04-13 14:10:20 -0700

    Moved function definition of execute into execute.hpp. Passed global variables by reference instead of using "extern" keyword.

--- main/execute.hpp
+++ main/execute.hpp
@@ -1,9 +1,28 @@
 #ifndef EXECUTE_HPP
 #define EXECUTE_HPP
 
-extern int alu_zero;
-extern int branch_target;
+void execute(int operand1, int operand2, int alu_ctrl, int sign_extension_offset, int pc_plus_4, int& alu_zero, int& branch_target) {
+    int result = 0;
 
-void Execute(int operand1, int operand2, int alu_ctrl, int sign_ext_offset, int pc_plus_4);
+    switch (alu_ctrl) {
+        case 0b0000: result = operand1 & operand2; 
+        break;
+        case 0b0001: result = operand1 | operand2; 
+        break;
+        case 0b0010: result = operand1 + operand2; 
+        break;
+        case 0b0110: result = operand1 - operand2; 
+        break;
+        case 0b0111: result = (operand1 < operand2) ? 1 : 0; 
+        break;
+        case 0b1100: result = ~(operand1 | operand2); 
+        break;
+        default: result = 0; 
+        break;
+    }
+
+    alu_zero = (result == 0);
+    branch_target = pc_plus_4 + (sign_extension_offset << 1);
+}
 
 #endif
