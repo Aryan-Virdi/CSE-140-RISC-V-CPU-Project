bb124ae5b9a193261e57fbe8b7a5cc5ea06180df
Aryan Virdi <avirdi2@ucmerced.edu>
2026-04-13 22:48:55 -0700

    Changed sign_extension_offset to alu_intake_2, because this parameter's source is enforced and maintained in main.cpp.

--- main/execute.hpp
+++ main/execute.hpp
@@ -1,7 +1,7 @@
 #ifndef EXECUTE_HPP
 #define EXECUTE_HPP
 
-int execute(int alu_intake_1, int alu_intake_2, int alu_control, int sign_extension_offset, int pc_plus_4, int& alu_zero, int& branch_target) {
+int execute(int alu_intake_1, int alu_intake_2, int alu_control, int pc_plus_4, int& alu_zero, int& branch_target) {
     int alu_result = 0;
 
     switch (alu_control) {
@@ -35,7 +35,7 @@ int execute(int alu_intake_1, int alu_intake_2, int alu_control, int sign_extens
     }
 
     alu_zero = (alu_result == 0);
-    branch_target = pc_plus_4 + (sign_extension_offset << 1);   
+    branch_target = pc_plus_4 + (alu_intake_2 << 1);   
 
     return alu_result;
 }
