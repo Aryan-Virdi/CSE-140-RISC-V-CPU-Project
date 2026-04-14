acb2d76b11fdb53cd48c36dacc3a38550f893108
Aryan Virdi <avirdi2@ucmerced.edu>
2026-04-13 22:46:33 -0700

    Changed total_clock_cycles to by-reference int parameter so that global variable from main can be used.

--- main/writeback.hpp
+++ main/writeback.hpp
@@ -1,9 +1,7 @@
 #ifndef WRITEBACK_HPP
 #define WRITEBACK_HPP
 
-int total_clock_cycles = 0; // Global variable to track total clock cycles
-
-void writeback(int alu_result, int mem_read_data, bool reg_write, bool mem_to_reg, int &write_data) {
+void writeback(int alu_result, int mem_read_data, bool reg_write, bool mem_to_reg, int &write_data, int& total_clock_cycles) {
     if (reg_write) {
         write_data = mem_to_reg ? mem_read_data : alu_result;
         total_clock_cycles++; // Increment clock cycles for writeback stage
