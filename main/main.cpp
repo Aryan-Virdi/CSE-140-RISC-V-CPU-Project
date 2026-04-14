b8d6f130b9cbbdca23c9824b1804bdbe81b27fca
Aryan Virdi <avirdi2@ucmerced.edu>
2026-04-14 00:26:34 -0700

    Passed instruction's immediate field unconditionally to execute().

--- main/main.cpp
+++ main/main.cpp
@@ -109,8 +109,7 @@ int main(int argc, char* argv[]) {
         int operand2 = (static_cast<bool>(ALUSrc) ? instruction.getImm() : instruction.getRs2Value());  // Second operand of ALU operation is from immediate if ALUSrc is true, otherwise from rs2.
                                                                                                         // Logic depends on ALUSrc being true if and only if the instruction is an I-Type.
 
-        int signExtendedRelativeOffset = ((instruction.getFormat() == Format::SB) ? instruction.getImm() : 0);
-        int aluResult = execute(operand1, operand2, alu_ctrl, signExtendedRelativeOffset, PC, alu_zero, branch_target);
+        int aluResult = execute(operand1, operand2, alu_ctrl, instruction.getImm(), PC, alu_zero, branch_target);
 
         int data = mem(d_mem, aluResult, instruction.getRs2Value(), static_cast<bool>(memWrite), printQueue);   // Returns an actual d_mem value if memWrite is true.
                                                                                                                 // The value in this function call is the second source register because
