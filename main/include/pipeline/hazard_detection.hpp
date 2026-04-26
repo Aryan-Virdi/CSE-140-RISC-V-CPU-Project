67aa2e9709b84f32e56bda3c525a6a164f933d6f
Aryan Virdi <avirdi2@ucmerced.edu>
2026-04-26 15:48:35 -0700

    Renamed variables to better show difference in stages.

--- main/include/pipeline/hazard_detection.hpp
+++ main/include/pipeline/hazard_detection.hpp
@@ -16,10 +16,10 @@ class HazardDetectionUnit{
     // If true, stall pipeline.
     bool stallPipeline(){
         bool memReadAtEXE = static_cast<bool>(id_exe_reg->getMemRd());
-        int rd = id_exe_reg->getRd();
-        int rs1 = if_id_reg->getRs1();
-        int rs2 = if_id_reg->getRs2();
-        bool destRegIsAnOperand = ((rd == rs1 || rd == rs2));
+        int id_rd = id_exe_reg->getRd();
+        int if_rs1 = if_id_reg->getRs1();
+        int if_rs2 = if_id_reg->getRs2();
+        bool destRegIsAnOperand = ((id_rd == if_rs1 || id_rd == if_rs2));
 
         return (memReadAtEXE && destRegIsAnOperand);
     }
