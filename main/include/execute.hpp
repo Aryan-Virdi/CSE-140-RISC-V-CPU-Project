f4a937fde0e4eb384c78331e3c509a38394ab85c
jalam01 <162780469+jalam01@users.noreply.github.com>
2026-04-26 16:23:56 -0700

    Update execute.hpp

--- main/include/execute.hpp
+++ main/include/execute.hpp
@@ -1,63 +1,55 @@
 #ifndef EXECUTE_HPP
 #define EXECUTE_HPP
 
-int execute(int alu_intake_1, int alu_intake_2, int alu_control, int sign_extension_offset, int pc_plus_4, int& alu_zero, int& branch_target, int pc_current, bool is_jal, bool is_jalr, int& jump) {
+int execute(int alu_intake_1, int alu_intake_2, int alu_control, int sign_extension_offset, int pc_current, int& alu_zero, int& branch_target, bool is_jal, bool is_jalr, int alu_result) {
     int alu_result = 0;
 
     switch (alu_control) {
         case 0b0000: 
-            alu_result = alu_intake_1 & alu_intake_2; 
-            break; // AND
+        alu_result = alu_intake_1 & alu_intake_2; 
+        break; // AND
 
         case 0b0001: 
-            alu_result = alu_intake_1 | alu_intake_2; 
-            break; // OR
+        alu_result = alu_intake_1 | alu_intake_2; 
+        break; // OR
 
         case 0b0010: 
-            alu_result = alu_intake_1 + alu_intake_2; 
-            break; // ADD
+        alu_result = alu_intake_1 + alu_intake_2; 
+        break; // ADD
 
         case 0b0110: 
-            alu_result = alu_intake_1 - alu_intake_2; 
-            break; // SUB
+        alu_result = alu_intake_1 - alu_intake_2; 
+        break; // SUB
 
         case 0b0111: 
-            alu_result = (alu_intake_1 < alu_intake_2) ? 1 : 0; 
-            break; // SLT
+        alu_result = (alu_intake_1 < alu_intake_2); 
+        break; // SLT
 
         case 0b1100: 
-            alu_result = ~(alu_intake_1 | alu_intake_2); 
-            break; // NOR
+        alu_result = ~(alu_intake_1 | alu_intake_2); 
+        break; // NOR
 
         default: 
-            alu_result = 0; 
-            break; // UNDEFINED
+        alu_result = 0; 
+        break; // UNDEFINED
     }
 
     alu_zero = (alu_result == 0);
-    branch_target = pc_plus_4 + (sign_extension_offset << 1);  
-    
+
+    // Branch targeting for normal branches
+    branch_target = pc_current + (sign_extension_offset << 1);
+
+    // JAL is for pc based jumping
     if (is_jal) {
-        jump = pc_current + sign_extension_offset;
+        branch_target = pc_current + sign_extension_offset;
     }
 
+    // JALR is for register based jumps
     if (is_jalr) {
-        jump = (alu_intake_1 + sign_extension_offset) & ~1;
+        branch_target = (alu_intake_1 + sign_extension_offset) & ~1;
     }
 
     return alu_result;
 }
 
-// Branch target:
-// Is always computed here.
-// The fetch stage will choose between PC+4 and this value
-// based on the branch control signal and alu_zero
-
-// sign_extension_offset:
-// Comes specifically from the instruction’s immediate field
-// Is always sign-extended
-// Used only for branch address calculation
-
-// alu_intake_2 -> goes into the ALU
-// sign_extension_offset -> goes into branch address logic
 #endif
