ac432ffeff427777c06078b9e7e4c752fd100280
jalam01 <162780469+jalam01@users.noreply.github.com>
2026-05-02 23:40:20 -0700

    Update hazard_detection.hpp

--- main/include/pipeline/hazard_detection.hpp
+++ main/include/pipeline/hazard_detection.hpp
@@ -1,1 +1,28 @@
+#ifndef HAZARD_DETECTION_HPP
+#define HAZARD_DETECTION_HPP
 
+#include "pipeline_registers.hpp"
+
+class HazardDetectionUnit{
+    IF_ID* if_id_reg;
+    ID_EXE* id_exe_reg;
+
+    public:
+    HazardDetectionUnit(IF_ID* if_id_reg, ID_EXE* id_exe_reg){
+        this->if_id_reg = if_id_reg;
+        this->id_exe_reg = id_exe_reg;
+    }
+
+    // If true, stall pipeline.
+    bool stallPipeline(){
+        bool memReadAtEXE = static_cast<bool>(id_exe_reg->getMemRd());
+        int id_rd = id_exe_reg->getRd();
+        int if_rs1 = if_id_reg->getRs1();
+        int if_rs2 = if_id_reg->getRs2();
+        bool destRegIsAnOperand = ((id_rd == if_rs1 || id_rd == if_rs2));
+
+        return (memReadAtEXE && destRegIsAnOperand);
+    }
+};
+
+#endif
