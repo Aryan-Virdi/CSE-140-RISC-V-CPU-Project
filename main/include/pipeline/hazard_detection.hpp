0c772bc9f9a03d0bbd5be383acaf2781f038618c
jalam01 <162780469+jalam01@users.noreply.github.com>
2026-05-02 21:07:08 -0700

    Update hazard_detection.hpp

--- main/include/pipeline/hazard_detection.hpp
+++ main/include/pipeline/hazard_detection.hpp
@@ -3,25 +3,50 @@
 
 #include "pipeline_registers.hpp"
 
-class HazardDetectionUnit{
-    IF_ID* if_id_reg;
-    ID_EXE* id_exe_reg;
+class HazardDetectionUnit {
+    IF_ID*   if_id_reg;
+    ID_EXE*  id_exe_reg;
+    EXE_MEM* exe_mem_reg;
+}
 
-    public:
+public:
     HazardDetectionUnit(IF_ID* if_id_reg, ID_EXE* id_exe_reg){
         this->if_id_reg = if_id_reg;
         this->id_exe_reg = id_exe_reg;
     }
 
-    // If true, stall pipeline.
-    bool stallPipeline(){
-        bool memReadAtEXE = static_cast<bool>(id_exe_reg->getMemRd());
-        int id_rd = id_exe_reg->getRd();
-        int if_rs1 = if_id_reg->getRs1();
-        int if_rs2 = if_id_reg->getRs2();
-        bool destRegIsAnOperand = ((id_rd == if_rs1 || id_rd == if_rs2));
+    bool stallPipeline() {
+        int  if_rs1 = if_id_reg->getRs1();
+        int  if_rs2 = if_id_reg->getRs2();
+        bool useRs2 = if_id_reg->useRs2();
 
-        return (memReadAtEXE && destRegIsAnOperand);
+        // Load-use hazard: 1 stall
+        if (id_exe_reg->getMemRd()) {
+            int  load_dest = id_exe_reg->getRd();
+            bool hazard    = (load_dest != 0) && ((load_dest == if_rs1) || (useRs2 && load_dest == if_rs2));
+            if (hazard){
+                return true;
+            }
+        }
+
+        // RAW hazard stall 1: producing instruction in EX stage
+        if (id_exe_reg->getRegWr() && !id_exe_reg->getMemRd()) {
+            int  exe_dest = id_exe_reg->getRd();
+            bool hazard   = (exe_dest != 0) && ((exe_dest == if_rs1) || (useRs2 && exe_dest == if_rs2));
+            if (hazard) {
+                return true;
+            }
+        }
+
+        // RAW hazard stall 2: producing instruction in MEM stage
+        if (exe_mem_reg->getRegWr()) {
+            int  mem_dest = exe_mem_reg->getRd();
+            bool hazard   = (mem_dest != 0) && ((mem_dest == if_rs1) || (useRs2 && mem_dest == if_rs2));
+            if (hazard) {
+                return true;
+            }
+
+        return false;
     }
 };
 
