607e84955670a41e34ebb7ff96f9e8dcc8ed505e
Aryan Virdi <avirdi2@ucmerced.edu>
2026-05-04 12:14:22 -0700

    "Undid" last commit since changes were not going where I wanted them to.

--- main/include/fetch.hpp
+++ main/include/fetch.hpp
@@ -24,9 +24,8 @@
 uint32_t fetch(int PC, int& nextPC, int& currPC, const std::vector<uint32_t>& instructionMemory, PrintEvent& printQueue, std::string& terminateFlag){
     int memoryIndex = PC / 4;
     if (!(memoryIndex < instructionMemory.size())){;
-        // std::cerr << "Exit Code: " << ERROR << std::endl;
-        // throw std::runtime_error("Segmentation Fault: Out-of-Bounds memory accessed");
-        terminateFlag = "TERMINATE";
+        std::cerr << "Exit Code: " << ERROR << std::endl;
+        throw std::runtime_error("Segmentation Fault: Out-of-Bounds memory accessed");
     }
     
     uint32_t instruction = instructionMemory[memoryIndex];
