d1309e52102714ce419cd415614fb4dc7861811d
Aryan Virdi <avirdi2@ucmerced.edu>
2026-05-04 23:30:12 -0700

    Rewrote loop body for pipelinedCPU(). First instruction appears to execute correctly.

--- main/main.cpp
+++ main/main.cpp
@@ -105,7 +105,7 @@ void singleCycleCPU(){
         /* MEM STAGE END */
         /* WRITEBACK STAGE BEGIN */
 
-        writeback(aluResult, data, static_cast<bool>(regWrite), static_cast<bool>(memToReg), rf, instruction.getRd(), total_clock_cycles, printQueue);
+        writeback(aluResult, data, static_cast<bool>(regWrite), static_cast<bool>(memToReg), rf, instruction.getRd(), printQueue);
         total_clock_cycles++;
         /* WRITEBACK STAGE END */
 
@@ -124,8 +124,77 @@ void pipelinedCPU(){
 
     HazardDetectionUnit hazardDetectionUnit(&if_id_buffer, &id_exe_buffer);
 
-    while(!pipelineDrained(currPC, instructionMemory.size(), if_id_buffer, id_exe_buffer, exe_mem_buffer, mem_wb_buffer)){
-        
+    while(!pipelineDrained(PC, instructionMemory.size(), if_id_buffer, id_exe_buffer, exe_mem_buffer, mem_wb_buffer)){
+        /* WRITEBACK BEGIN */
+        if (mem_wb_buffer.getValid()){
+            writeback(mem_wb_buffer.getALUResult(), mem_wb_buffer.getMemData(), mem_wb_buffer.getRegWr(), mem_wb_buffer.getMemToReg(), rf, mem_wb_buffer.getRd(), printQueue);
+        }
+        /* WRITEBACK END */
+
+        /* MEM BEGIN */
+        if (exe_mem_buffer.getValid()){
+            int data = mem(d_mem, exe_mem_buffer.getALUResult(), exe_mem_buffer.getRs2Value(), exe_mem_buffer.getMemWr(), printQueue);
+            mem_wb_buffer.updateInfo(exe_mem_buffer, data);
+            mem_wb_buffer.updateValid(true);
+        } else {
+            mem_wb_buffer.updateValid(false);
+        }
+        /* MEM END */
+
+        /* EXE BEGIN */
+        if (id_exe_buffer.getValid()){
+            int alu_ctrl = aluControl(id_exe_buffer.getALUOp(), id_exe_buffer.getFunct3(), id_exe_buffer.getFunct7());
+            int aluResult = execute(    
+                                        id_exe_buffer.getOp1(),
+                                        id_exe_buffer.getOp2(), alu_ctrl,
+                                        id_exe_buffer.getImm(),
+                                        id_exe_buffer.getPC(),
+                                        id_exe_buffer.getNextPC(),
+                                        alu_zero,
+                                        id_exe_buffer.getBranch(),
+                                        id_exe_buffer.getJump(),
+                                        id_exe_buffer.getPCSrc(),
+                                        id_exe_buffer.getRs1(),
+                                        branch_target
+                                    );
+            if (static_cast<bool>(id_exe_buffer.getBranch()) && static_cast<bool>(alu_zero) || static_cast<bool>(id_exe_buffer.getJump())) { PC = branch_target; }
+            // printQueue.addPrintEvent(LocationType::programCounter, EMPTY_IDX, PC);
+
+            exe_mem_buffer.updateInfo(id_exe_buffer, alu_zero, aluResult, branch_target);
+            exe_mem_buffer.updateValid(true);
+        } else {
+            exe_mem_buffer.updateValid(false);
+        }
+        /* EXE END */
+
+        /* DECODE BEGIN */
+        if (if_id_buffer.getValid()){
+            Instruction instruction = decode(if_id_buffer.getInstr(), controlSignals, rf);
+            int operand1 = (static_cast<bool>(ALUSrc2) ? if_id_buffer.getNextPC() : instruction.getRs1Value());
+            int operand2 = (static_cast<bool>(ALUSrc) ? instruction.getImm() : instruction.getRs2Value()); 
+
+            id_exe_buffer.updateInfo(if_id_buffer, controlSignals, instruction.getRd(), operand1, operand2, instruction.getImm(), instruction.getFunct3(), instruction.getFunct7(), instruction.getRs2Value());
+            id_exe_buffer.updateValid(true);
+        } else {
+            id_exe_buffer.updateValid(false);
+        }
+        /* DECODE END */
+
+        /* FETCH BEGIN */
+        if ((PC/4) < instructionMemory.size()){
+            uint32_t currInstruction = fetch(PC, nextPC, currPC, instructionMemory, printQueue);
+            if_id_buffer.updateInfo(PC, nextPC, currInstruction);
+            if_id_buffer.updateValid(true);
+            PC = nextPC;
+        } else {
+            if_id_buffer.updateValid(false);
+        }
+        printQueue.addPrintEvent(LocationType::programCounter, EMPTY_IDX, PC);
+        /* FETCH END */
+
+        total_clock_cycles++;
+        cout << "total_clock_cycles " << total_clock_cycles << " :" << endl;
+        printQueue.printModifications(withConventionalNames);    // Print this cycle's modifications.
     }
 }
 
